597b6a3c151b9d72d5f76131b4b13ed6b6671c0d
KaroRonty <karo.ronty@gmail.com>
2019-10-05 12:04:49 +0200

    Made the plot logarithmic, formatting

--- StockMarketLSTM.R
+++ StockMarketLSTM.R
@@ -1,12 +1,12 @@
-library(ggplot2)
-library(readr)
-library(tibble)
 library(dplyr)
 library(tidyr)
 library(keras)
+library(tibble)
+library(ggplot2)
 library(httr) # downloading the xls(x) files
 library(readxl) # reading xls(x) files
-library(dplyr) # data formatting
+
+# Load the price data for the S&P 500
 source("https://raw.githubusercontent.com/KaroRonty/ShillerGoyalDataRetriever/master/ShillerGoyalDataRetriever.r")
 
 # Set the training set and test set size
@@ -16,20 +16,20 @@ test_size <- 500
 # Set the amount of lags in months, the batch size and epochs
 lag_n <- 12 * 5
 batch_size <- 3
-epochs <- 2
+epochs <- 10
 
 # Get a specified amount of rows and select columns
-data <- full_data %>% 
-    slice((nrow(full_data) - (train_size + test_size) + 1):nrow(full_data)) %>% 
-    mutate(dates = as.Date(paste0(dates, "-01"))) %>%
-    select(dates, P)
+data <- full_data %>%
+  slice((nrow(full_data) - (train_size + test_size) + 1):nrow(full_data)) %>%
+  mutate(dates = as.Date(paste0(dates, "-01"))) %>%
+  select(dates, P)
 
 # Split into training and test sets ----
-train_unscaled <- data %>% 
-    slice(1:train_size)
+train_unscaled <- data %>%
+  slice(1:train_size)
 
-test_unscaled <- data %>% 
-    slice((train_size + 1):(train_size + test_size))
+test_unscaled <- data %>%
+  slice((train_size + 1):(train_size + test_size))
 
 # Difference training and test set values
 # TODO: percentages instead of plain differences
@@ -38,15 +38,15 @@ test_differenced <- diff(test_unscaled %>% pull(P))
 
 # Functions for normalizing and denormalizing values
 normalize <- function(x) {
-    (x - min(train_differenced, na.rm = TRUE)) /
-        (max(train_differenced, na.rm = TRUE) -
-             min(train_differenced, na.rm = TRUE))
+  (x - min(train_differenced, na.rm = TRUE)) /
+    (max(train_differenced, na.rm = TRUE) -
+       min(train_differenced, na.rm = TRUE))
 }
 
 denormalize <- function(x) {
-    x * (max(train_differenced, na.rm = TRUE) -
-             min(train_differenced, na.rm = TRUE)) +
-        min(train_differenced, na.rm = TRUE)
+  x * (max(train_differenced, na.rm = TRUE) -
+         min(train_differenced, na.rm = TRUE)) +
+    min(train_differenced, na.rm = TRUE)
 }
 
 # Normalize the training and test sets
@@ -73,14 +73,15 @@ dim(test_x) <- c(nrow(test_x), ncol(test_x), 1)
 # Make the model ----
 model <- keras_model_sequential()
 
-model %>% 
-    layer_lstm(units = 4, input_shape = c(lag_n, 1)) %>% 
-    layer_dense(units = 1)
+model %>%
+  layer_lstm(units = 4,
+             input_shape = c(lag_n, 1)) %>%
+  layer_dense(units = 1)
 
-model %>% 
-    compile(loss = "mse",
-            optimizer = "adam",
-            metrics = "mae")
+model %>%
+  compile(loss = "mse",
+          optimizer = "adam",
+          metrics = "mae")
 
 # Print the loss and accuracy while training the model
 history <- model %>% fit(x = train_x,
@@ -98,33 +99,33 @@ pred_test <- predict(model, test_x, batch_size = 1)
 pred_train <- denormalize(pred_train)
 pred_test <- denormalize(pred_test)
 
-# Revert the differencing
+# Revert the differencing for the training and test sets
 pred_train_undiff <- pred_train +
-    train_unscaled %>% 
-    slice((lag_n + 1):(dim(train_unscaled)[1] - 1)) %>% 
-    pull(P)
+  train_unscaled %>%
+  slice((lag_n + 1):(dim(train_unscaled)[1] - 1)) %>%
+  pull(P)
 
 pred_test_undiff <- pred_test +
-    test_unscaled %>% 
-    slice((lag_n + 1):(dim(test_unscaled)[1] - 1)) %>% 
-    pull(P)
+  test_unscaled %>%
+  slice((lag_n + 1):(dim(test_unscaled)[1] - 1)) %>%
+  pull(P)
 
 # Make a data frame containing the correctly scaled actuals and predictions
-res <- tibble(time_id = c(train_unscaled$dates, test_unscaled$dates),
+res <- tibble(dates = c(train_unscaled$dates, test_unscaled$dates),
               train = c(train_unscaled$P, rep(NA, dim(test_unscaled)[1])),
               test = c(rep(NA, dim(train_unscaled)[1]), test_unscaled$P),
               pred_train = c(rep(NA, lag_n + 1),
                              pred_train_undiff,
                              rep(NA, dim(test_unscaled)[1])),
               pred_test = c(rep(NA, dim(train_unscaled)[1]),
-                            rep(NA, lag_n+1),
+                            rep(NA, lag_n + 1),
                             pred_test_undiff))
 
 # Gather for plotting
 res2 <- res %>%
-    gather(key = 'data', value = 'P', train:pred_test)
+  gather(key = "data", value = "P", train:pred_test)
 
 # Plot the actuals and predictions
-ggplot(res2, aes(x = time_id,
-                 y = P)) +
-    geom_line(aes(color = data))
+ggplot(res2, aes(x = dates,
+                 y = log(P))) +
+  geom_line(aes(color = data))
